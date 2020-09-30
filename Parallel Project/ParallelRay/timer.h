fb14ad7f4591ba575a1e3faba336d7108f8a7234
sillysaburo <72171998+sillysaburo@users.noreply.github.com>
2020-09-30 16:33:41 -0700

    Add files via upload

---
+++ Parallel Project/ParallelRay/timer.h
@@ -0,0 +1,30 @@
+
+
+
+/* COMP 137-1 Spring 2018
+ * filename: time.h
+ *
+ * Functions to get current time.
+ *
+ * getProcessTime() returns time in seconds since the start of the program.
+ * getWorldTime() returns time in seconds since the Epoch (00:00:00 UTC, January 1 1970)
+ *
+ * getProcessTime() seems to be having problems for some users, particularly OSX users.
+ */
+
+#include <time.h>
+/*inline*/ double getProcessTime()
+{
+    clock_t t;
+    t = clock();
+    return ((double)t)/CLOCKS_PER_SEC;
+}
+
+#include <sys/time.h>
+/*inline*/ double getWorldTime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    double s = t.tv_sec + ((double)t.tv_usec)/1000000.0;
+    return s;
+}
